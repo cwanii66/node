9ca2d74bd92e21c9765ca32f5dc729782e839ef0
Chris Wong <wh18710455626@126.com>
2022-01-02 22:29:59 +0800

    add crypto

--- NBlog/src/controller/user.js
+++ NBlog/src/controller/user.js
@@ -1,7 +1,10 @@
 const { execute, escape } = require('../db/mysql');
+const { genPassword } = require('../utils/cryp');
 
 const login = async (username, password) => {
     username = escape(username);
+    // 生成加密密码
+    password = genPassword(password);
     password = escape(password);
 
     const sql_login = `
