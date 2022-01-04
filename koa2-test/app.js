cd105f569f1bf12a7169340bf36e63bd3a3b37e6
Chris Wong <wh18710455626@126.com>
2022-01-04 19:56:54 +0800

    finish api

--- koa2-test/app.js
+++ koa2-test/app.js
@@ -5,15 +5,21 @@ const json = require('koa-json')
 const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
+const session = require('koa-generic-session');
+const redisStore = require('koa-redis');
 
-const index = require('./routes/index')
-const users = require('./routes/users')
+// const index = require('./routes/index')
+// const users = require('./routes/users')
+const blog = require('./routes/blog');
+const user = require('./routes/user');
+
+const { REDIS_CONFIG } = require('./config/db');
 
 // error handler
 onerror(app)
 
 // middlewares
-app.use(bodyparser({
+app.use(bodyparser({ // 协助解析中间件
   enableTypes:['json', 'form', 'text']
 }))
 app.use(json())
@@ -32,9 +38,27 @@ app.use(async (ctx, next) => {
   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
 })
 
+// session 配置
+app.keys = ['fads_da23a83'];
+app.use(session({
+  // 配置cookie
+  cookie: {
+    path: '/',
+    httpOnly: true,
+    maxAge: 24 * 60 * 60 * 1000
+  },
+  // 配置redis
+  store: redisStore({
+    // all: '127.0.0.1:6379' // 写死本地redis
+    all: `${REDIS_CONFIG.host}:${REDIS_CONFIG.port}`
+  })
+})); // session => 符合koa2规范的中间件函数 => 类似于原始版本的简易session封装，session() return 的 function 也为我们封装对应的session模型
+
 // routes
-app.use(index.routes(), index.allowedMethods())
-app.use(users.routes(), users.allowedMethods())
+// app.use(index.routes(), index.allowedMethods())
+// app.use(users.routes(), users.allowedMethods())
+app.use(blog.routes(), blog.allowedMethods());
+app.use(user.routes(), user.allowedMethods());
 
 // error-handling
 app.on('error', (err, ctx) => {
---
+++ koa2-test/model/resModel.js
@@ -0,0 +1,34 @@
+class BaseModel {
+    constructor(data, msg) {
+        if (typeof data === 'string') {
+            this.msg = data;
+            data = null;
+            msg = null;
+        }
+        if (data) {
+            this.data = data;
+        }
+        if (msg) {
+            this.msg = msg;
+        }
+    }
+}
+
+class SuccessModel extends BaseModel {
+    constructor(data, msg) {
+        super(data, msg);
+        this.errno = 0;
+    }
+}
+
+class ErrorModel extends BaseModel {
+    constructor(data, msg) {
+        super(data, msg);
+        this.errno = -1;
+    }
+}
+
+module.exports = {
+    SuccessModel,
+    ErrorModel
+}
