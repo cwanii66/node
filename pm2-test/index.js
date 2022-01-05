4e5314df4b11578bd71bdd1a3561c035002dfae5
Chris Wong <wh18710455626@126.com>
2022-01-05 09:10:13 +0800

    add pm2.config.json

--- pm2-test/index.js
+++ pm2-test/index.js
@@ -15,7 +15,7 @@ const server = http.createServer((req, res) => {
     res.end(
         JSON.stringify({
             errno: 0,
-            msg: 'pm2 test server 3'
+            msg: 'pm2 test server 6'
         })
     )
 })
